c1273b791cb943cc53fc689599cae51f27756525
Juan <juampi.parzybal@gmail.com>
2022-05-23 16:43:56 -0300

    primerEjercicio

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,14 +1,14 @@
 'use strict'
-
+//1001 = 1x2*3 + 0x2*2 + 0x2*1 + 1x2*0
 function BinarioADecimal(num) {
-  while (x) {
-    
+  let decimal = 0;
+  for (let i = 0; i < num.length; i++) {
+    decimal = decimal + num[i] * 2 ** (num.length -1 - i);
   }
-
+ return decimal;
 }
-
 function DecimalABinario(num) {
-  // tu codigo aca
+
 
 }
 
